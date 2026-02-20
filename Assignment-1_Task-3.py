35c591abd795eb5c3d7a648ef5889448f4a27c94
pcdeb <gangulidebojit97@gmail.com>
2026-02-20 10:19:27 +0530

    Assignment-1_Task-3.py

---
+++ Assignment-1_Task-3.py
@@ -0,0 +1,22 @@
+#file operations
+
+import os
+
+def write_to_file(file_name):
+    with open(file_name,"+at") as f:
+        f.write("\nNew content is being written")
+
+
+
+if os.path.exists("demofile.txt"):
+    write_to_file("demofile.txt")
+
+else: 
+    with open("demofile.txt","+x") as f:
+        f.write("Created a new file")
+
+    
+f = open("demofile.txt","+r")
+f.read()
+
+f.close()
